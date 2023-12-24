99b374029cfd42f8d6858fff3b54d6ddc2f153d2
thefidanabdulla <thefidanabdulla@gmail.com>
2023-12-24 14:10:35 +0400

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import Layout from '@/components/layout/Layout'
+import dynamic from 'next/dynamic';
+
 import '@/styles/globals.css'
+// import Layout from '@/components/layout/Layout'
+const Layout = dynamic(import('@/components/layout/Layout'), { ssr: false })
 
 export default function App({ Component, pageProps }) {
   return (
